5f88de4645b2c79019eb48d2756cd1dd2385f3fd
Rdiss03 <moudjibourouchdan80@gmail.com>
2024-12-18 21:27:09 +0100

    building the front images for production and local env purpose

--- .ci/build-publish.sh
+++ .ci/build-publish.sh
@@ -2,11 +2,21 @@
 : "${REPO:?REPO not set or empty}"
 : "${TAG:?TAG not set or empty}"
 
+ARG_WITH_AWS_API_BASE_URL="$2"
+ARG_WITH_LOCAL_BACKEND_API_BASE_URL="$3"
+
 docker buildx create --use
 docker buildx build \
     --platform=linux/amd64,linux/arm64 \
     -t "${USERNAME}/${REPO}:${TAG}" \
     -t "${USERNAME}/${REPO}:latest" \
-    "${@:2}" \
+    "${ARG_WITH_AWS_API_BASE_URL}" \
+    --push \
+    "$1"
+
+docker buildx build \
+    --platform=linux/amd64,linux/arm64 \
+    -t "${USERNAME}/${REPO}:${TAG}_local" \
+    "${ARG_WITH_LOCAL_BACKEND_API_BASE_URL}" \
     --push \
     "$1"
