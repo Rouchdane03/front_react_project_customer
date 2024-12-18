cd461fe184bd1f4d580a394f261245f2391254eb
Rdiss03 <moudjibourouchdan80@gmail.com>
2024-12-18 22:49:41 +0100

    update the script shell file argument

--- .ci/build-publish.sh
+++ .ci/build-publish.sh
@@ -10,13 +10,13 @@ docker buildx build \
     --platform=linux/amd64,linux/arm64 \
     -t "${USERNAME}/${REPO}:${TAG}" \
     -t "${USERNAME}/${REPO}:latest" \
-    "${ARG_WITH_AWS_API_BASE_URL}" \
+    --build-arg "api_base_url=${ARG_WITH_AWS_API_BASE_URL}" \
     --push \
     "$1"
 
 docker buildx build \
     --platform=linux/amd64,linux/arm64 \
     -t "${USERNAME}/${REPO}:${TAG}_local" \
-    "${ARG_WITH_LOCAL_BACKEND_API_BASE_URL}" \
+    --buil-arg "api_base_url=${ARG_WITH_LOCAL_BACKEND_API_BASE_URL}" \
     --push \
     "$1"
